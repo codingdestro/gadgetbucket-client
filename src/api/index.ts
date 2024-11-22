c6f9eaed67d30e68f39ad35c337a59a956bb34c1
codingdestro <codingdestro@gmail.com>
2024-11-22 18:42:58 +0530

    unkown

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ import cart from "./cart";
 import sign from "./signin";
 import orders from "./orders";
 
-axios.defaults.baseURL = import.meta.env.API_URL || "/api/";
+axios.defaults.baseURL = import.meta.env.API_URL || "http://127.0.0.1:5000/";
 const api = {
   products,
   cart,
