f02b332178c65a24655901faf9fb78217b92a784
codingdestro <codingdestro@gmail.com>
2025-08-08 17:50:44 +0530

    update: navbar links for cart and order

--- src/pages/ViewProduct.tsx
+++ src/pages/ViewProduct.tsx
@@ -20,7 +20,7 @@ export default function ViewProduct() {
   }, []); //eslint-disable-line react-hooks/exhaustive-deps
   return (
     <div>
-      <div className="max-w-6xl mx-auto p-4 lg:p-8">
+      <div className="max-w-6xl mx-auto p-4 lg:p-8 h-screen">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
           {/* Product Image */}
           <div className="flex justify-center">
